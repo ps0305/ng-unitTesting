cc56e138d0d1ace1845ecffc8c89d9c421378ec8
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-20 18:21:20 +0530

    spec.ts

---
+++ src/app/calc.spec.ts
@@ -0,0 +1,28 @@
+import {Calculator} from "./calc";
+
+//Jasmine start the execution from describle function
+describe("calculator testing",()=>{
+
+    //describe =inner describe function in order to write unit test cases
+    describe("increment function testing",()=>{
+        // "it"=to write test suit code
+        it("increment function increments the count by 1",()=>{
+                //objection to calculator
+                var obj:Calculator = new Calculator();
+            const result=obj.increment();
+            expect(result).toBe(1);
+        });
+    });
+
+    //decrement function
+    describe("decrement function testing",()=>{
+        it("decrement function decrements the count by 1",()=>{
+            var obj:Calculator = new Calculator();
+            const result=obj.decrement();
+            //expect result
+            expect(result).toBe(-1)//developer result=tester result=-1
+            
+        })
+    })
+})
+
